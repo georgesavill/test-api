42185752c040f82264cc6207b94b1f8a00506098
georgesavill <gpsavill+github@gmail.com>
2020-12-08 11:01:15 +0000

    Add project files.

---
+++ test-api/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("TestController.Get() called");
+            return Environment.GetEnvironmentVariable("TEST-API-HOST");
+        }
+    }
+}
